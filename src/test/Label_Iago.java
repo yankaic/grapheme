d1bf2b74cb3c9ba487a1981916d2119bf7dfcaf1
Yan Kaic <Yan Kaic@Pindaiba>
2016-03-12 15:40:22 -0300

    novos paineis redimensionaveis

---
+++ src/test/Label_Iago.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Yan Kaic
+ */
+public class Label_Iago extends JLabel{
+  
+}
