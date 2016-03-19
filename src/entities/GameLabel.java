16db1dffdd0d36f2246324db178e5e20395735e0
shanks <shanks@shanks>
2016-03-19 16:48:14 -0300

    implementação do Resize
    Iago

--- src/entities/GameLabel.java
+++ src/entities/GameLabel.java
@@ -7,8 +7,10 @@ import javax.swing.JLabel;
  * @author shanks
  */
 public class GameLabel {
+    private GameObject object;
+    private JLabel labelObject;
     
     public GameLabel(JLabel label){
-        
+        this.labelObject=label;
     }//fim construtor
 }//fim class
